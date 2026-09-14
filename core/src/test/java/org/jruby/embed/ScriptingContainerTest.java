1c71e4d1753b1dccf9209676741905a1996948f1
Amin Mansuri <aminm@gatech.edu>
2026-09-14 20:25:57 -0300

    Test that other values of the sharing property are ignored
    "0", "yes" or a typo turned sharing off silently.

--- core/src/test/java/org/jruby/embed/ScriptingContainerTest.java
+++ core/src/test/java/org/jruby/embed/ScriptingContainerTest.java
@@ -325,6 +325,23 @@ public class ScriptingContainerTest {
         }
     }
 
+    @Test
+    public void testSharingVariablesSystemPropertyIgnoresOtherValues() {
+        System.setProperty(AttributeName.SHARING_VARIABLES.toString(), "0");
+        try {
+            ScriptingContainer instance = new ScriptingContainer(LocalContextScope.SINGLETHREAD);
+            try {
+                assertNull(instance.getAttribute(AttributeName.SHARING_VARIABLES));
+                instance.put("@message", "shared");
+                assertEquals("shared", instance.runScriptlet("@message"));
+            } finally {
+                instance.terminate();
+            }
+        } finally {
+            System.clearProperty(AttributeName.SHARING_VARIABLES.toString());
+        }
+    }
+
     /**
      * Test of getAttribute method, of class ScriptingContainer.
      */
